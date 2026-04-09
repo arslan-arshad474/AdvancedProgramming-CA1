191d947e32616b3115c350b96b32cbdc082558b6
Arslan Arshad <20093854@mydbs.ie>
2026-04-09 23:45:51 +0100

    update

--- Task1/ChurrosShop.Tests/obj/Debug/net6.0/ChurrosShop.Tests.AssemblyInfo.cs
+++ Task1/ChurrosShop.Tests/obj/Debug/net6.0/ChurrosShop.Tests.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ChurrosShop.Tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+383d793a0a883b2c1827a101c3fa6f8f8db732fd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f76b701ef374d2ebe7c878df56d8b8a27b61e076")]
 [assembly: System.Reflection.AssemblyProductAttribute("ChurrosShop.Tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ChurrosShop.Tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
